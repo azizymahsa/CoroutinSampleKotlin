4d770e15912d751399b6c3cda230146e5e2d6807
Mahsa Azizy <37101768+azizymahsa@users.noreply.github.com>
2024-04-09 19:57:07 -0700

    Initial commit

---
+++ app/src/main/java/com/example/coroutinsamplekotlin/SecondActivity.kt
@@ -0,0 +1,16 @@
+package com.example.coroutinsamplekotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.coroutinsamplekotlin.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
